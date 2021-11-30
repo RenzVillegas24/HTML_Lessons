14e3a9a6b9b8fa2119ebf6935cde82485d262863
RenzVillegas24 <renzjustinevillegas@gmail.com>
2021-12-01 02:29:44 +0800

    main

---
+++ pages/Tests/input.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  $fullname = $_POST['fullname'];
+  $password = $_POST['password'];
+
+  echo "Full Name: ".$fullname."<br>Password: ".$password."<br>";
+  print_r($_POST);  
+?>
+
+</body>
+</html>
